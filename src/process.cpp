b97ef03694b5e75661d4fa72e5f865757f84df35
Dymarenko <dymarenko76@gmail.com>
2016-06-22 19:34:53 +0300

    Mini

--- src/process.cpp
+++ src/process.cpp
@@ -4,6 +4,23 @@
 Process::Process(){
 	pid = getPid();
 }
-int Process::getPid(){
-	
+int Process::getPid(char* Name){
+	PROCESSENTRY32 ProcessEntry;
+	HANDLE pHandle;
+	DWORD pid;
+	pHandle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	ProcessEntry.dwSize = sizeof(ProcessEntry);
+	pid=0;
+	bool loop=Process32First(pHandle, &ProcessEntry);
+	while (loop)
+	{
+		AnsiString nm=ProcessEntry.szExeFile;
+		if (nm==Name)
+		{
+		pid = ProcessEntry.th32ProcessID;
+		CloseHandle(pHandle);
+		break;
+		}
+		loop=Process32Next(pHandle, &ProcessEntry);
+	}
 }
